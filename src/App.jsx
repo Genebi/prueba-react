407b488d0b16fb886fb533f60074017e69895c3d
Alejandro <alejandro.medina@territorium.com>
2024-06-10 22:39:44 -0600

    Se puede mostrar la cantidad especificada de Cards por fila

--- src/App.jsx
+++ src/App.jsx
@@ -34,22 +34,64 @@ function App() {
       description: "Directo de las guerras clon",
       image: "https://v.etsystatic.com/video/upload/q_auto/Comp13_yuhbdx.jpg",
     },
+    {
+      id: 6,
+      name: "Captain Rex",
+      description: "Directo de las guerras clon",
+      image: "https://m.media-amazon.com/images/I/41yOgAgy41L._AC_.jpg",
+    },
+    {
+      id: 7,
+      name: "Comannder Cody",
+      description: "Directo de las guerras clon",
+      image: "https://v.etsystatic.com/video/upload/q_auto/Comp13_yuhbdx.jpg",
+    },
+    {
+      id: 8,
+      name: "Darth Vader",
+      description: "Directo de las guerras clon",
+      image:
+        "https://images.stockx.com/images/Hasbro-Star-Wars-The-Black-Series-Darth-Vader-Premium-Electronic-Helmet.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1652496759",
+    },
+    {
+      id: 9,
+      name: "Captain Rex Clone",
+      description: "Directo de las guerras clon",
+      image: "https://m.media-amazon.com/images/I/41yOgAgy41L._AC_.jpg",
+    },
+    {
+      id: 10,
+      name: "Comannder Cody Clone",
+      description: "Directo de las guerras clon",
+      image: "https://v.etsystatic.com/video/upload/q_auto/Comp13_yuhbdx.jpg",
+    },
   ];
+
+  const chunkArray = (arr, size) => {
+    const result = [];
+    for (let i = 0; i < arr.length; i += size) {
+      result.push(arr.slice(i, i + size));
+    }
+    return result;
+  };
+
+  const chunkedProducts = chunkArray(products, 6);
 
   return (
     <>
       <Layout />
 
       <div className="container mt-5">
-        {products.map((product) => (
-          <div className="row">
-            <div className="col-md-3 mb-3" key={product.id}>
+        {chunkedProducts.map((chunk, index) => (
+          <div className="row" key={index}>
+            {chunk.map((product) => (
               <Card
+                key={product.id}
                 name={product.name}
                 description={product.description}
                 image={product.image}
               />
-            </div>
+            ))}
           </div>
         ))}
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,32 +1,54 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export function Card({name, description, image}) {
+export function Card({ name, description, image }) {
+  const [quantity, setQuantity] = useState(0);
 
-    const [quantity, setQuantity] = useState(0)
+  const addProduct = () => {
+    setQuantity(quantity + 1);
+  };
 
-    const addProduct = () => {
-        setQuantity(quantity + 1)
-    }
+  const removeProduct = () => {
+    let newQuantity = quantity < 1 ? 0 : quantity - 1;
+    setQuantity(newQuantity);
+  };
 
-    const removeProduct = () => {
-        
-        let newQuantity = quantity < 1 ? 0 : quantity - 1
-        setQuantity(newQuantity)
-    }
-
-    return (
-        <div className="card">
-            <div className="card-body text-center">
-                <img className="img-fluid" src={image} style={{width: '320px', height: '350px'}} />
-                <h5 className="card-title mt-1">{name}</h5>
-                <p className="card-text">{description}</p>
-                <div className="mt-2">
-                    <button type="button" className="btn btn-danger" onClick={removeProduct}>-</button>
-                    <input type="text" className="form-control d-inline text-center m-1" readOnly value={quantity} style={{width: '50px'}} />
-                    <button type="button" className="btn btn-success" onClick={addProduct}>+</button>
-                </div>
-                <a className="btn btn-primary">Agregar al carrito</a>
-            </div>
+  return (
+    <div className="col-md-2 mb-3">
+      <div className="card">
+        <div className="card-body text-center">
+          <img
+            className="img-fluid"
+            src={image}
+            style={{ width: "320px", height: "350px" }}
+          />
+          <h5 className="card-title mt-1">{name}</h5>
+          <p className="card-text">{description}</p>
+          <div className="mt-2">
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={removeProduct}
+            >
+              -
+            </button>
+            <input
+              type="text"
+              className="form-control d-inline text-center m-1"
+              readOnly
+              value={quantity}
+              style={{ width: "50px" }}
+            />
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={addProduct}
+            >
+              +
+            </button>
+          </div>
+          <a className="btn btn-primary">Agregar al carrito</a>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
